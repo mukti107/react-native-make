57fd8cefae326d80b4fc4d6e4ee81374124d5c85
Rachindra Poudel <mukti107@gmail.com>
2022-08-10 21:39:13 +0545

    use existsSync as check

--- react-native.config.js
+++ react-native.config.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 const { resolve } = require('path');
-const { lstatSync } = require('fs');
+const { lstatSync, existsSync } = require('fs');
 
 /**
  * For faster development workflow, when npm linked
  * we want this library to be transpiled at runtime using the ts-node resolver
  */
-const res = lstatSync(resolve('..', 'node_modules', '@evlop', 'react-native-make'));
-if (res.isSymbolicLink()) {
-  console.warn('Detected linked install of react-native-make, compiling at runtime...');
-  require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
-  module.exports = require('./src/rn-plugin.config').rnPluginConfig;
-} else {
-  module.exports = require('./dist/rn-plugin.config.js').rnPluginConfig;
+
+const reactNativeMakePath = resolve('.', 'node_modules', '@evlop', 'react-native-make');
+
+if (existsSync(reactNativeMakePath)) {
+  const res = lstatSync(resolve('.', 'node_modules', '@evlop', 'react-native-make'));
+  if (res.isSymbolicLink()) {
+    console.warn('Detected linked install of react-native-make, compiling at runtime...');
+    require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
+    module.exports = require('./src/rn-plugin.config').rnPluginConfig;
+  } else {
+    module.exports = require('./dist/rn-plugin.config.js').rnPluginConfig;
+  }
 }
